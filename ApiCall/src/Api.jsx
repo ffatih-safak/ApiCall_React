e53a18699fec83194b6ab30289c3a5247417fe4f
ffsafak <64497939+ffatih-safak@users.noreply.github.com>
2024-02-13 22:38:36 +0300

    Update Api.jsx

--- ApiCall/src/Api.jsx
+++ ApiCall/src/Api.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'; // Add this line to import axios
 const searchImages = async (term) => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
       headers: {
-        Authorization: 'Client-ID diBkm8NMtj4sDt_FDjc_E3MUyGXfObYtDDzHn5nxsQ4',
+        Authorization: 'Client-ID clientKey!!',
       },
       params: {
         query: term,
